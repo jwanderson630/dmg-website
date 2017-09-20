6071ca59207fcc7e2c3114ca2d0fe2ed2253f482
Joe <yoej262@gmail.com>
2017-09-20 11:01:29 -0700

    updates

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -1,10 +1,22 @@
 var keystone = require('keystone');
 var Enquiry = keystone.list('Enquiry');
+var nodemailer = require('nodemailer');
 
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	const transporter = nodemailer.createTransport({
+		host: 'smtp-mail.outlook.com',
+		port: 587,
+		secure: false,
+		auth: {
+			user: 'janderson@dunthorpemarketing.com',
+			pass: 'Earth28'
+		}
+	});
+
+
 
 	// Set locals
 	locals.section = 'contact';
@@ -12,11 +24,23 @@ exports = module.exports = function (req, res) {
 	locals.validationErrors = {};
 	locals.gate = true;
 
+
 	// On POST requests, add the Enquiry item to the database
 	view.on('post', { action: 'contact' }, function (next) {
 
 		var newEnquiry = new Enquiry.model();
 		var updater = newEnquiry.getUpdateHandler(req);
+		const mailOptions = {
+			from: '<janderson@dunthorpemarketing.com>',
+			to: '<jwdunthorpe@gmail.com',
+			text: 'test'
+		};
+		transporter.sendMail(mailOptions, (error,info) => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log(info.messageId);
+		});
 
 		updater.process(req.body, {
 			flashErrors: true,
