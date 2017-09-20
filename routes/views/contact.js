e0092392c12dc071cdafd2aaea7b81280c240ef2
Joe <yoej262@gmail.com>
2017-09-20 11:38:37 -0700

    'update'

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -7,12 +7,12 @@ exports = module.exports = function (req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 	const transporter = nodemailer.createTransport({
-		host: 'smtp-mail.outlook.com',
+		host: 'in-v3.mailjet.com',
 		port: 587,
 		secure: false,
 		auth: {
-			user: 'janderson@dunthorpemarketing.com',
-			pass: 'Earth28'
+			user: 'e7793244bd880bdb4c02d5d47710f2d3',
+			pass: 'a7a2f167c4c38a2542da53ea1e697ae4'
 		}
 	});
 
